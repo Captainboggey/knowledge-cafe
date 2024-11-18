76d798b64346fc468eb51c131fc78aa78950ca00
captainboogey <anik35-700@diu.edu.bd>
2024-11-19 05:37:37 +0600

    blog added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 
-
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
+        <header className='flex justify-between items-center p-4 m-4 border-b-2'>
              <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+             <img src={profile} alt="" />
             
-        </div>
+        </header>
     );
 };
 
