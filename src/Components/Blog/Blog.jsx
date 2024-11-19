40b9c01d3fd1b25670ae16684808f0053f92ef89
captainboogey <anik35-700@diu.edu.bd>
2024-11-19 11:06:00 +0600

    adding blogs

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,8 +2,30 @@ import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
     console.log(blog)
+    const {title,cover_img,reading_time,author_name,author_img,published_date,hashtag}=blog
     return (
         <div>
+            <img className='w-96' src={cover_img} alt="" />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                  <img className='w-14' src={author_img} alt="" />
+                  <div className='ml-6'>
+                    <h3 className='text-2xl'>{author_name}</h3>
+                    <p>{published_date}</p>
+                  </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+
+
+                </div>
+            </div>
+            <h1>{title}</h1>
+            <p>
+                {
+                    hashtag.map((hash,idx)=> <span key={idx}><a href=''>  {hash}</a></span>)
+                }
+            </p>
             
         </div>
     );
