628bb916e0df70dc049e4fb21eab582d57289857
captainboogey <anik35-700@diu.edu.bd>
2024-11-19 12:03:17 +0600

    bookmark added

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
+import { CiBookmarkPlus } from "react-icons/ci";
 
-const Blog = ({blog}) => {
-    console.log(blog)
+const Blog = ({blog,handleAddToBookmark}) => {
+    // console.log(blog)
     const {title,cover_img,reading_time,author_name,author_img,published_date,hashtag}=blog
     return (
-        <div>
-            <img className='w-96' src={cover_img} alt="" />
-            <div className='flex justify-between'>
+        <div className='mt-20'>
+            <img className='w-full mb-8 rounded-2xl' src={cover_img} alt="" />
+            <div className='flex justify-between mb-4'>
                 <div className='flex'>
-                  <img className='w-14' src={author_img} alt="" />
+                  <img className='w-14 ' src={author_img} alt="" />
                   <div className='ml-6'>
                     <h3 className='text-2xl'>{author_name}</h3>
                     <p>{published_date}</p>
@@ -16,6 +17,7 @@ const Blog = ({blog}) => {
                 </div>
                 <div>
                     <span>{reading_time} min read</span>
+                    <button onClick={()=>handleAddToBookmark(blog)} className='ml-2 text-2xl text-red-600'><CiBookmarkPlus></CiBookmarkPlus></button>
 
 
                 </div>
@@ -30,7 +32,5 @@ const Blog = ({blog}) => {
         </div>
     );
 };
-Blog.propTypes={
-    blog: PropTypes.object.isRequired
-}
+
 export default Blog;
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import Blog from "../Blog/Blog";
 
 
-const Blogs = () => {
+
+const Blogs = ({handleAddToBookmark}) => {
     const [blogs,setBlogs]=useState([]);
       
 
@@ -15,11 +16,13 @@ const Blogs = () => {
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
             {
-                blogs.map(blog=><Blog key={blog.id} blog={blog}></Blog>)
+                blogs.map(blog=><Blog handleAddToBookmark={handleAddToBookmark} key={blog.id} blog={blog}></Blog>)
             }
             
         </div>
     );
 };
+
+
 
 export default Blogs;
